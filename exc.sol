7da971a2e3e81974f8c2c9faf7d76f18d617298e
Adonis Valamontes <tony@geopay.me>
2018-04-30 14:05:00 +0300

    Create exc.sol

---
+++ exc.sol
@@ -0,0 +1,90 @@
+pragma solidity ^0.4.4;
+// ----------------------------------------------------------------------------
+// 'EXC' 'Example EXC Token' token contract
+//
+// Symbol      : EXC
+// Name        : Energy Unit
+// Total supply: 8888800000
+// Decimals    : 0
+//
+// 
+//
+// (c) by A. Valamontes with doecoins / Geopay.me Inc 2018. The MIT Licence.
+// ----------------------------------------------------------------------------
+//
+//contract tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData); }
+//
+
+contract EXC {
+    /* Public variables of the token */
+    string public standard = 'Token 0.1';
+    string public version = 'H1.0';       //human 0.1 standard. Just an arbitrary versioning scheme.
+
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
+    uint256 public initialSupply;
+
+    /* This creates an array with all balances */
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
+
+    /* This generates a public event on the blockchain that will notify clients */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /* Initializes contract with initial supply tokens to the creator of the contract */
+    function EXC() {
+
+        initialSupply = 8888800000;
+        totalSupply = 8888800000;                        // Update total supply (100000 for example)
+        balanceOf[msg.sender] = initialSupply;              // Give the creator all initial tokens
+        totalSupply = initialSupply;                        // Update total supply
+        name = "Energy Unit";                                   // Set the name for display purposes
+        symbol = "EXC";                               // Set the symbol for display purposes
+        decimals = 0;                            // Amount of decimals for display purposes
+    }
+
+    /* Send coins */
+    function transfer(address _to, uint256 _value) {
+        if (balanceOf[msg.sender] < _value) throw;           // Check if the sender has enough
+        if (balanceOf[_to] + _value < balanceOf[_to]) throw; // Check for overflows
+        balanceOf[msg.sender] -= _value;                     // Subtract from the sender
+        balanceOf[_to] += _value;                            // Add the same to the recipient
+        Transfer(msg.sender, _to, _value);                   // Notify anyone listening that this transfer took place
+    }
+
+    /* Allow another contract to spend some tokens in your behalf */
+    function approve(address _spender, uint256 _value)
+        returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        return true;
+    }
+
+    /* Approve and then communicate the approved contract in a single tx 
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData)
+        returns (bool success) {
+        tokenRecipient spender = tokenRecipient(_spender);
+        if (approve(_spender, _value)) {
+            spender.receiveApproval(msg.sender, _value, this, _extraData);
+            return true;
+        }
+    } */       
+
+    /* A contract attempts to get the coins */
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+        if (balanceOf[_from] < _value) throw;                 // Check if the sender has enough
+        if (balanceOf[_to] + _value < balanceOf[_to]) throw;  // Check for overflows
+        if (_value > allowance[_from][msg.sender]) throw;   // Check allowance
+        balanceOf[_from] -= _value;                          // Subtract from the sender
+        balanceOf[_to] += _value;                            // Add the same to the recipient
+        allowance[_from][msg.sender] -= _value;
+        Transfer(_from, _to, _value);
+        return true;
+    }
+
+    /* This unnamed function is called whenever someone tries to send ether to it */
+    function () {
+        throw;     // Prevents accidental sending of ether
+    }
+}
